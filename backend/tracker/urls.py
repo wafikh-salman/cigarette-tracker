c5ea3b6ceb4ed747c0d462ea2a7e1b3f92803ec5
wafikh salman <wafikhw@gmail.com>
2026-09-25 11:38:14 +0530

    feat:implemented dashboard

--- backend/tracker/urls.py
+++ backend/tracker/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import BrandApiView,CiggretteEntriesApiView
+from .views import BrandApiView,CiggretteEntriesApiView,DashboardApiView
 urlpatterns = [
     path('brand/',BrandApiView.as_view(),name="brand"),
     path('brand/<int:pk>/',BrandApiView.as_view(),name="brandwithid"),
     path('entries/',CiggretteEntriesApiView.as_view(),name="entires"),
-    path('entries/<int:pk>/',CiggretteEntriesApiView.as_view(),name="entry")
-    
+    path('entries/<int:pk>/',CiggretteEntriesApiView.as_view(),name="entry"),
+    path('dashboard/',DashboardApiView.as_view(),name="dashboard"),
 ]
--- backend/tracker/views.py
+++ backend/tracker/views.py
@@ -5,6 +5,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Brand,CigaretteEntry
 from django.shortcuts import get_object_or_404
+from django.db.models import Avg, Count, Max, Min, Sum,F
+from django.utils import timezone
+from datetime import timedelta
+
 # Create your views here.
 class BrandApiView(APIView):
     def get(self,request,**kwargs):
@@ -81,14 +85,58 @@ class CiggretteEntriesApiView(APIView):
     
     def patch(self,request,**kwargs):
        entry = get_object_or_404(CigaretteEntry,pk=kwargs['pk'])
-       serializer = CigaretteEntrySerializer(entry,data=request.data,partial=True)
-       if serializer.is_valid():
-           serializer.save()
+       if request.data.get('action') == "increase":
+           entry.quantity+=1
+           entry.save()
+           serializer = CigaretteEntrySerializer(entry)
+           return Response(serializer.data,status=status.HTTP_200_OK)
+           
+       elif request.data.get('action') == 'decrease':
+           if entry.quantity == 1:
+               return Response({'error':"quantity must be above 1"})
+
+           entry.quantity-=1
+           entry.save()
+           serializer = CigaretteEntrySerializer(entry)
            return Response(serializer.data,status=status.HTTP_200_OK)
        
-       return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+       else:
+           return Response({"error":"action should be increase or decrease"},status=status.HTTP_400_BAD_REQUEST)
+       
    
-   
+class DashboardApiView(APIView):
+    def get(self,request):
+        today = timezone.now().date()
+        entries = CigaretteEntry.objects.filter(
+        created_at__date=today)
+        last_7_days = timezone.now().date() - timedelta(days=7)
+        total_quantity = entries.aggregate(
+            total_quantity=Sum('quantity')
+            )['total_quantity'] or 0
+           
+        total_spending = entries.aggregate(
+            total_spending=Sum(F('quantity') * F('brand__price'))
+        )['total_spending'] or 0
+        
+        week_entries = CigaretteEntry.objects.filter(created_at__date=last_7_days)
+        week_quantity = week_entries.aggregate(
+            week_quantity=Sum('quantity')
+            )['week_quantity'] or 0
+        
+        week_spending = week_entries.aggregate(
+            week_spending=Sum(
+                F('quantity') * F('brand__price'))
+                )['week_spending'] or 0
+        
+        return Response({
+            "total_quantity":total_quantity,
+            "total_spending":total_spending,
+            'week_quantity':week_quantity,
+            'week_spending':week_spending
+        },status=status.HTTP_200_OK)
+        
+        
+        
     
     
     
